fb7fbbccb4accffe4e4dec2b82dc0296a11e15d9
Trisha <trishachaudhary41@gmail.com>
2025-01-21 17:50:42 +0545

    debug

--- sakhiBackend/sakhiBackend/settings.py
+++ sakhiBackend/sakhiBackend/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 SECRET_KEY = 'django-insecure-v-#1y3)ajdaytnz^@m%$e$j%+*2s!sdf!#hzpym&e7$_hx-&3='
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 SITE_ID = 1
